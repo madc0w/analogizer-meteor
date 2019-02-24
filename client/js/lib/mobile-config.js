b1321b76d2d461525444e4a8e07dd9322535b2ca
Marc A. Donis <mad7@runbox.com>
2019-02-24 11:49:26 +0100

    .

---
+++ client/js/lib/mobile-config.js
@@ -0,0 +1,8 @@
+App.icons({
+	// Android
+	android_mdpi : "resources/icons/mipmap-mdpi/ic_launcher.png",
+	android_hdpi : "resources/icons/mipmap-hdpi/ic_launcher.png",
+	android_xhdpi : "resources/icons/mipmap-xhdpi/ic_launcher.png",
+	android_xxhdpi : "resources/icons/mipmap-xxhdpi/ic_launcher.png",
+	android_xxxhdpi : "resources/icons/mipmap-xxxhdpi/ic_launcher.png",
+});
