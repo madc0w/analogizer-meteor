e5b5a15744c3ba34068405662f2708820307a30e
Marc A. Donis <marc.a.donis@primeresults.lu>
2019-02-24 12:50:14 +0100

    .

---
+++ scripts/build-android.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+meteor build --verbose ~/analogizer-build --server=http://localhot:3000
+
+cd ~/analogizer-build/android/project/build/outputs/apk/release
+
+jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 android-release-unsigned.apk analogizer
+
+$ANDROID_HOME/build-tools/23.0.1/zipalign 4 android-release-unsigned.apk signed.apk
+
