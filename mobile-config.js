ed90b8e78bec9ccbee2aadabf658e660692a0bd5
Marc A. Donis <mad7@runbox.com>
2019-02-24 13:42:47 +0100

    .

--- mobile-config.js
+++ mobile-config.js
@@ -6,3 +6,18 @@ App.icons({
 	android_xxhdpi : "resources/icons/mipmap-xxhdpi/ic_launcher.png",
 	android_xxxhdpi : "resources/icons/mipmap-xxxhdpi/ic_launcher.png",
 });
+
+
+App.launchScreens({
+	// Android
+	android_mdpi_portrait : "resources/splash/drawable-mdpi/background.9.png",
+	android_mdpi_landscape : "resources/splash/drawable-mdpi/background.9.png",
+	android_hdpi_portrait : "resources/splash/drawable-hdpi/background.9.png",
+	android_hdpi_landscape : "resources/splash/drawable-hdpi/background.9.png",
+	android_xhdpi_portrait : "resources/splash/drawable-xhdpi/background.9.png",
+	android_xhdpi_landscape : "resources/splash/drawable-xhdpi/background.9.png",
+	android_xxhdpi_portrait : "resources/splash/drawable-xxhdpi/background.9.png",
+	android_xxhdpi_landscape : "resources/splash/drawable-xxhdpi/background.9.png",
+	android_xxxhdpi_portrait : "resources/splash/drawable-xxxhdpi/background.9.png",
+	android_xxxhdpi_landscape : "resources/splash/drawable-xxxhdpi/background.9.png",
+});
